bc20a67b8b0e300010c5133d98b2a8ac48320010
lingam321 <56913057+lingam321@users.noreply.github.com>
2021-03-17 01:11:37 +0530

    ss

---
+++ FinanceEntryBook/app/src/main/java/com/crazy/financeentrybook/login.java
@@ -0,0 +1,14 @@
+package com.crazy.financeentrybook;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
